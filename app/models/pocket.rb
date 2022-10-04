10d798fd188cb67404477aa687f7179523bd828d
Camilla Poushyk <camilla.poushyk@gmail.com>
2022-10-04 11:17:25 -0400

    Changes to equipment 3

--- app/models/pocket.rb
+++ app/models/pocket.rb
@@ -2,11 +2,5 @@ class Pocket < ApplicationRecord
 
   belongs_to :holder, polymorphic: true
   belongs_to :item
-  def nil_equipment
-      check = Equipment.find_by(pocket_id: self.id)
-      if (check)
-          check.unequip
-      end
-  end
 
 end
