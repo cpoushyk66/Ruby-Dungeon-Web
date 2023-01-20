867482cba2b384082e752e4a255d3bfe5e1aab3e
cpoushyk66 <charlotte.poushyk@gmail.com>
2023-01-19 23:08:36 -0500

    Remaking dungeon creation usingv new styling

--- client/src/components/DungeonRoom.js
+++ client/src/components/DungeonRoom.js
@@ -1,10 +1,11 @@
 import React, {useState, useEffect} from "react";
+import { DungeonBottom, DungeonCenter, DungeonContainer, DungeonCornerBottomLeft, DungeonCornerBottomRight, DungeonCornerTopLeft, DungeonCornerTopRight, DungeonLeft, DungeonRight, DungeonTop, PlayerSpace } from "../styles/DungeonTileManager";
 import Battle from "./Battle";
 
 function DungeonRoom({character, updateCurrentCharacter}) {
 
     const [dungeon, setDungeon] = useState(null)
-    const [position, setPosition] = useState(null)
+    const [position, setPosition] = useState([0, 0])
     const [moves, setMoves] = useState(0)
     const [battle, setBattle] = useState(false)
     const [enemies, setEnemies] = useState(0)
@@ -39,8 +40,11 @@ function DungeonRoom({character, updateCurrentCharacter}) {
 
     let index = 0
     function makeDungeon() {
-       return dungeon.dungeon.map(row => <tr key={index += 1} >{row.map(tile => formatRoom(tile))}</tr>)
+       return dungeon.dungeon.map(row => <tr key={index += 1} >{row.map(tile => determineCell(tile.coords, Math.sqrt(dungeon.rooms)))}</tr>)
     }
+
+    // return dungeon.dungeon.map(row => <tr key={index += 1} >{row.map(tile => formatRoom(tile))}</tr>)   determineCell(tile.coords, Math.sqrt(dungeon.rooms))
+
 
     function movePlayer(newCoords, dungeon) {
         setPosition([clamp(newCoords[0], 0, Math.sqrt(dungeon.rooms) - 1), clamp(newCoords[1], 0, Math.sqrt(dungeon.rooms) - 1)])
@@ -69,16 +73,16 @@ function DungeonRoom({character, updateCurrentCharacter}) {
     function formatRoom(room) {
         switch (room.room_type) {
             case "empty_room":
-                return <td key={index += 1} className={`empty_room ${setTileClass(room.coords, Math.sqrt(dungeon.rooms))} ${room.visited ? "visited" : "unvisited"}`}></td>
+                return <td key={index += 1} className={`empty_room ${setTileClass(room.coords, Math.sqrt(dungeon.rooms))} ${room.visited ? "visited" : "unvisited"}`}>{room.coords[0] == position[0] && room.coords[1] == position[1] ? <PlayerSpace /> : null}</td>
                 break
             case "enemy_room":
-                return <td key={index += 1} className={`enemy_room ${setTileClass(room.coords, Math.sqrt(dungeon.rooms))} ${room.visited ? "visited" : "unvisited"}`}></td>
+                return <td key={index += 1} className={`enemy_room ${setTileClass(room.coords, Math.sqrt(dungeon.rooms))} ${room.visited ? "visited" : "unvisited"}`}>{room.coords[0] == position[0] && room.coords[1] == position[1] ? <PlayerSpace /> : null}</td>
                 break
             case "item_room":
-                return <td key={index += 1} className={`item_room ${setTileClass(room.coords, Math.sqrt(dungeon.rooms))} ${room.visited ? "visited" : "unvisited"}`}></td>
+                return <td key={index += 1} className={`item_room ${setTileClass(room.coords, Math.sqrt(dungeon.rooms))} ${room.visited ? "visited" : "unvisited"}`}>{room.coords[0] == position[0] && room.coords[1] == position[1] ? <PlayerSpace /> : null}</td>
                 break
             case "boss_room":
-                return <td key={index += 1} className={`boss_room ${setTileClass(room.coords, Math.sqrt(dungeon.rooms))} ${room.visited ? "visited" : "unvisited"}`}></td>
+                return <td key={index += 1} className={`boss_room ${setTileClass(room.coords, Math.sqrt(dungeon.rooms))} ${room.visited ? "visited" : "unvisited"}`}>{room.coords[0] == position[0] && room.coords[1] == position[1] ? <PlayerSpace /> : null}</td>
                 break
         }
 
@@ -106,9 +110,32 @@ function DungeonRoom({character, updateCurrentCharacter}) {
             return "dungeon-center"
     }
 
+    function determineCell(coords, sideLength) {
+        if (coords[0] == 0 && coords[1] == 0) {
+            return <DungeonCornerTopLeft visited={dungeon.dungeon[coords[0]][coords[1]].visited} >{coords[0] == position[0] && coords[1] == position[1] ? <PlayerSpace /> : null}</DungeonCornerTopLeft>
+        }
+        else if (coords[0] == 0 && coords[1] == sideLength - 1)
+            return <DungeonCornerTopRight visited={dungeon.dungeon[coords[0]][coords[1]].visited}>{coords[0] == position[0] && coords[1] == position[1] ? <PlayerSpace /> : null}</DungeonCornerTopRight>
+        else if (coords[0] == 0 && coords[1] != 0 && coords[1] != sideLength - 1)
+            return <DungeonTop visited={dungeon.dungeon[coords[0]][coords[1]].visited}>{coords[0] == position[0] && coords[1] == position[1] ? <PlayerSpace /> : null}</DungeonTop>
+        else if (coords[0] == sideLength - 1 && coords[1] == 0)
+            return <DungeonCornerBottomRight visited={dungeon.dungeon[coords[0]][coords[1]].visited}>{coords[0] == position[0] && coords[1] == position[1] ? <PlayerSpace /> : null}</DungeonCornerBottomRight>
+        else if (coords[0] == sideLength - 1 && coords[1] == sideLength - 1)
+            return <DungeonCornerBottomLeft visited={dungeon.dungeon[coords[0]][coords[1]].visited}>{coords[0] == position[0] && coords[1] == position[1] ? <PlayerSpace /> : null}</DungeonCornerBottomLeft>
+        else if (coords[0] == sideLength - 1)
+            return <DungeonBottom visited={dungeon.dungeon[coords[0]][coords[1]].visited}>{coords[0] == position[0] && coords[1] == position[1] ? <PlayerSpace /> : null}</DungeonBottom>
+        else if (coords[1] == 0)
+            return <DungeonLeft visited={dungeon.dungeon[coords[0]][coords[1]].visited}>{coords[0] == position[0] && coords[1] == position[1] ? <PlayerSpace /> : null}</DungeonLeft>
+        else if (coords[1] == sideLength - 1)
+            return <DungeonRight visited={dungeon.dungeon[coords[0]][coords[1]].visited}>{coords[0] == position[0] && coords[1] == position[1] ? <PlayerSpace /> : null}</DungeonRight>
+        else 
+            return <DungeonCenter visited={dungeon.dungeon[coords[0]][coords[1]].visiteds}>{coords[0] == position[0] && coords[1] == position[1] ? <PlayerSpace /> : null}</DungeonCenter>
+    }
+
     return(
         <div className="dungeon-actual">
-            {!battle && dungeon != null ? <table cellPadding={0} cellSpacing={0}>
+            {!battle && dungeon != null ? 
+            <DungeonContainer cellPadding={0} cellSpacing={0}>
                 <thead>
                     <tr>
                         <td colSpan={dungeon != null ? Math.sqrt(dungeon.rooms) : 1}>Dungeon</td>
@@ -121,7 +148,7 @@ function DungeonRoom({character, updateCurrentCharacter}) {
                 <tbody>
                     {dungeon != null ? makeDungeon() : null}
                 </tbody>
-            </table> : <Battle handleBattleWon={handleBattleWon} character={character} enemies={enemies != null ? enemies : []}  updateCurrentCharacter={updateCurrentCharacter} difficulty={difficulty}/>}
+            </DungeonContainer> : <Battle handleBattleWon={handleBattleWon} character={character} enemies={enemies != null ? enemies : []}  updateCurrentCharacter={updateCurrentCharacter} difficulty={difficulty}/>}
 
             {!battle ? <div>
                 <button onClick={() => movePlayer([position[0], position[1] + 1], dungeon)}>→</button>
@@ -129,6 +156,23 @@ function DungeonRoom({character, updateCurrentCharacter}) {
                 <button onClick={() => movePlayer([position[0] + 1, position[1]], dungeon)}>↓</button>
                 <button onClick={() => movePlayer([position[0] - 1, position[1]], dungeon)}>↑</button>
             </div> : null}
+
+            <DungeonContainer cellPadding={0} cellSpacing={0}>
+                <tbody>
+                    <tr>
+                        <DungeonCornerTopLeft visited={true}> <PlayerSpace /></DungeonCornerTopLeft> <DungeonTop></DungeonTop> <DungeonTop /> <DungeonTop /> <DungeonTop /> <DungeonTop /> <DungeonTop /> <DungeonTop /> <DungeonTop /> <DungeonCornerTopRight />
+                    </tr>
+                    <tr>
+                        <DungeonLeft/> <DungeonCenter /> <DungeonCenter /><DungeonCenter /><DungeonCenter /><DungeonCenter /><DungeonCenter /><DungeonCenter /><DungeonCenter /> <DungeonRight />
+                    </tr>
+                    <tr>
+                        <DungeonLeft/> <DungeonCenter /> <DungeonCenter /><DungeonCenter><PlayerSpace rotatePlayer={90}/></DungeonCenter> <DungeonCenter /><DungeonCenter /><DungeonCenter /><DungeonCenter /><DungeonCenter /> <DungeonRight />
+                    </tr>
+                    <tr>
+                        <DungeonLeft/> <DungeonCenter /> <DungeonCenter /><DungeonCenter /><DungeonCenter /><DungeonCenter /><DungeonCenter /><DungeonCenter /><DungeonCenter /> <DungeonRight />
+                    </tr>
+                </tbody>
+            </DungeonContainer>
         </div>
     )
 }
